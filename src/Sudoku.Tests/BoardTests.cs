eeae68ddedb56bf0a654e842988e8d0f79252a9d
ElemarJR <elemar@procad.net>
2011-01-18 09:00:41 -0200

    First Commit

---
+++ src/Sudoku.Tests/BoardTests.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Sudoku.Tests
+{
+    [TestFixture]
+    public class BoardTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MakeMove_NumberRepetitionInRegion_ThrowsInvalidOperationException()
+        {
+            // arrange
+            Board b = new Board();
+            
+            // act
+            b.MakeMove(new Move(Columns.A, Rows.One, Values.Nine));
+            b.MakeMove(new Move(Columns.B, Rows.Two, Values.Nine));
+
+            // assert
+        }
+
+        [Test]
+        public void UnmakeMove_MakeAndUnmake9InA1_EmptyA1()
+        {
+            Board b = new Board();
+            
+            b.MakeMove(new Move(Columns.A, Rows.One, Values.Nine));
+            b.UnmakeMove(new Move(Columns.A, Rows.One, Values.Nine));
+
+            Assert.AreEqual(Values.None, b.GetCellValue(Columns.A, Rows.One));
+            Assert.AreEqual(Values.None, b.GetColumnValues(Columns.A));
+            Assert.AreEqual(Values.None, b.GetRowValues(Rows.One));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MakeMove_NumberRepetitionInLine_ThrowsInvalidOperationException()
+        {
+            // arrange
+            Board b = new Board();
+
+            // act
+            b.MakeMove(new Move(Columns.A, Rows.One, Values.Nine));
+            b.MakeMove(new Move(Columns.G, Rows.One, Values.Nine));
+
+            // assert
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MakeMove_NumberRepetitionInColumn_ThrowsInvalidOperationException()
+        {
+            // arrange
+            Board b = new Board();
+
+            // act
+            b.MakeMove(new Move(Columns.A, Rows.One, Values.Nine));
+            b.MakeMove(new Move(Columns.A, Rows.Seven, Values.Nine));
+
+            // assert
+        }
+
+        [Test]
+        public void GetCellValue_SettingCellTo9_Returns9()
+        {
+            // arrange
+            var b = new Board();
+            b.MakeMove(new Move(Columns.A, Rows.Seven, Values.Nine));
+            // act
+            var result = b.GetCellValue(Columns.A, Rows.Seven);
+            // assert
+            Assert.AreEqual(Values.Nine, result);
+        }
+
+        [Test]
+        public void GetRowValues_Setting7And9_Returns7And9()
+        {
+            // arrange
+            var v = new Board();
+            v.MakeMove(new Move(Columns.A, Rows.One, Values.One));
+            v.MakeMove(new Move(Columns.G, Rows.One, Values.Nine));
+
+            // act
+            var result = v.GetRowValues(Rows.One);
+
+            // assert
+            Assert.AreEqual(Values.One | Values.Nine, result);
+        }
+
+        [Test]
+        public void GetColumnValues_Setting7And9_Returns7And9()
+        {
+            // arrange
+            var v = new Board();
+            v.MakeMove(new Move(Columns.A, Rows.One, Values.One));
+            v.MakeMove(new Move(Columns.A, Rows.Seven, Values.Nine));
+
+            // act
+            var result = v.GetColumnValues(Columns.A);
+
+            // assert
+            Assert.AreEqual(Values.One | Values.Nine, result);
+        }
+
+        [Test]
+        public void GetRegionValues_Setting7And9_Returns7And9()
+        {
+            // arrange
+            var v = new Board();
+            v.MakeMove(new Move(Columns.A, Rows.One, Values.One));
+            v.MakeMove(new Move(Columns.C, Rows.Three, Values.Nine));
+            
+            // act
+            var result = v.GetRegionValues(Columns.B, Rows.Two);
+
+            // assert
+            Assert.AreEqual(Values.One | Values.Nine, result);
+        }
+
+        [Test]
+        public void GetValueOptions_A1InEmptyBoard_ReturnsAllValues()
+        {
+            // arrange
+            var v = new Board();
+            var expected = Values.One | Values.Two | Values.Three
+                | Values.Four | Values.Five | Values.Six
+                | Values.Seven | Values.Eight | Values.Nine;
+            
+            // act
+            var result = v.GetValueOptions(Columns.A, Rows.One)
+                .Aggregate((v1, v2) => v1 | v2)
+                ;
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test()]
+        public void Solve_Board1()
+        {
+            // arrange
+            var b = new Board();
+            b
+                .MakeMoves(
+                    new Move(Columns.A, Rows.One, Values.Six),
+                    new Move(Columns.B, Rows.One, Values.Four),
+                    new Move(Columns.C, Rows.One, Values.One),
+                    new Move(Columns.F, Rows.One, Values.Seven)
+                )
+                .MakeMoves(
+                    new Move(Columns.C, Rows.Two, Values.Two),
+                    new Move(Columns.D, Rows.Two, Values.Six),
+                    new Move(Columns.E, Rows.Two, Values.Eight),
+                    new Move(Columns.F, Rows.Two, Values.Three),
+                    new Move(Columns.G, Rows.Two, Values.Five)
+                )
+                .MakeMoves(
+                    new Move(Columns.A, Rows.Three, Values.Three),
+                    new Move(Columns.H, Rows.Three, Values.Six),
+                    new Move(Columns.I, Rows.Three, Values.Two)
+                )
+                .MakeMoves(
+                    new Move(Columns.B, Rows.Four, Values.Five),
+                    new Move(Columns.D, Rows.Four, Values.Seven),
+                    new Move(Columns.H, Rows.Four, Values.Nine),
+                    new Move(Columns.I, Rows.Four, Values.Three)
+                )
+                .MakeMoves(
+                    new Move(Columns.A, Rows.Five, Values.One),
+                    new Move(Columns.C, Rows.Five, Values.Seven),
+                    new Move(Columns.G, Rows.Five, Values.Eight),
+                    new Move(Columns.I, Rows.Five, Values.Six)
+                )
+                .MakeMoves(
+                    new Move(Columns.A, Rows.Six, Values.Two),
+                    new Move(Columns.B, Rows.Six, Values.Three),
+                    new Move(Columns.F, Rows.Six, Values.Five),
+                    new Move(Columns.H, Rows.Six, Values.Seven)
+                )
+                .MakeMoves(
+                    new Move(Columns.A, Rows.Seven, Values.Eight),
+                    new Move(Columns.B, Rows.Seven, Values.One),
+                    new Move(Columns.I, Rows.Seven, Values.Five)
+                )
+                .MakeMoves(
+                    new Move(Columns.C, Rows.Eight, Values.Four),
+                    new Move(Columns.D, Rows.Eight, Values.Nine),
+                    new Move(Columns.E, Rows.Eight, Values.Six),
+                    new Move(Columns.F, Rows.Eight, Values.One),
+                    new Move(Columns.G, Rows.Eight, Values.Three)
+                )
+                .MakeMoves(
+                    new Move(Columns.D, Rows.Nine, Values.Four),
+                    new Move(Columns.G, Rows.Nine, Values.One),
+                    new Move(Columns.H, Rows.Nine, Values.Two),
+                    new Move(Columns.I, Rows.Nine, Values.Nine)
+                );
+            
+            // act
+            var result = b.Solve();
+            
+            // assert
+            Assert.IsTrue(b.IsSolved);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Solve_Board2()
+        {
+            int[] board = new int[] 
+            {
+                0,0,1,0,0,0,0,9,0,
+                3,0,0,0,0,2,0,1,0,
+                0,0,9,1,0,0,4,0,6,
+                0,0,0,4,0,8,0,0,0,
+                9,3,0,0,0,0,0,2,7,
+                0,0,0,2,0,7,0,0,0,
+                5,0,2,0,0,1,9,0,0,
+                0,9,0,7,0,0,0,0,5,
+                0,6,0,0,0,0,3,0,0
+            };
+
+            var b = new Board(board);
+
+            // act
+            var result = b.Solve();
+
+            // assert
+            Assert.IsTrue(b.IsSolved);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ src/Sudoku/Board.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku
+{
+    public class Board
+    {
+        Values[,] _BoardValues = new Values[9, 9];
+        Values[] _RowValues = new Values[9];
+        Values[] _ColumnValues = new Values[9];
+        Values[,] _RegionValues = new Values[3, 3];
+        int _EmptySquaresCount = 81;
+
+        public Board()
+        {
+
+        }
+
+        public Board(int[] board) : this()
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    Columns c = (Columns)j;
+                    Rows r = (Rows)i;
+                    if (board[(i * 9) + j] != 0)
+                    {
+                        Values v = (Values)(1 << (board[(i * 9) + j] - 1));
+                        if (v != Values.None)
+                            this.MakeMove(new Move(c, r, v));
+                    }
+                }
+        }
+
+        public Board MakeMoves(params Move[] moves)
+        {
+            foreach (var move in moves)
+                this.MakeMove(move);
+
+            return this;
+        }
+
+        public Board MakeMove(Move move)
+        {
+            int row = (int)move.Row;
+            int column = (int)move.Column;
+
+            _BoardValues[column, row] = move.Value;
+            //int bit = (int) move.Value;
+
+            if ((_RowValues[row] & move.Value) == move.Value)
+                throw new ArgumentException(
+                    string.Format("Invalid move {2}.\nNumber {0}. \nAlready present in row {1}\n{3}",
+                    move.Value,
+                    move.Row,
+                    move.ToString(),
+                    this.ToString()
+                    ));
+
+            if ((_ColumnValues[column] & move.Value) == move.Value)
+                throw new ArgumentException(
+                    string.Format("Invalid move {2}.\nNumber {0} already present in column {1}\n{3}",
+                    move.Value,
+                    move.Column,
+                    move.ToString(),
+                    this.ToString()
+                    ));
+
+            if ((_RegionValues[column / 3, row / 3] & move.Value) == move.Value)
+                throw new ArgumentException(
+                    string.Format("Invalid move {1}.\nNumber {0} already present in this region\n{2}",
+                    move.Value,
+                    move.ToString(),
+                    this.ToString()
+                    ));
+
+            _RowValues[row] |= move.Value;
+            _ColumnValues[column] |= move.Value;
+            _RegionValues[column / 3, row / 3] |= move.Value;
+            _EmptySquaresCount--;
+
+            return this;
+        }
+
+        public Board UnmakeMove(Move move)
+        {
+            int row = (int)move.Row;
+            int column = (int)move.Column;
+            if (_BoardValues[column, row] == Values.None)
+                throw new ArgumentException();
+
+            _BoardValues[column, row] = Values.None;
+            int bit = ~((int)move.Value);
+            _RowValues[row] = (Values) ((int)_RowValues[row] & bit);
+            _ColumnValues[column] = (Values)((int)_ColumnValues[column] & bit);
+            _RegionValues[column / 3, row / 3] = (Values) ((int) _RegionValues[column / 3, row / 3] & bit);
+            _EmptySquaresCount++;
+
+            return this;
+        }
+
+        public Values GetCellValue(Columns column, Rows row)
+        {
+            return this._BoardValues[(int)column, (int)row];
+        }
+
+        public Values GetRowValues(Rows row)
+        {
+            return _RowValues[(int)row];
+        }
+
+        public Values GetRegionValues(Columns oneCellColumn, Rows oneCellRow)
+        {
+            int column = (int)oneCellColumn;
+            int row = (int)oneCellRow;
+            return _RegionValues[column / 3, row / 3];
+        }
+
+
+        public Values GetColumnValues(Columns column)
+        {
+            return _ColumnValues[(int)column];
+        }
+
+        public Values GetDeniedValues(Columns column, Rows row)
+        {
+            return GetRowValues(row) |
+                GetColumnValues(column) |
+                GetRegionValues(column, row);
+        }
+
+        public IEnumerable<Values> GetValueOptions(Columns column, Rows row)
+        {
+            var usedValues = GetDeniedValues(column, row);
+
+            for (int i = 0; i < 9; i++)
+            {
+                Values candidate = (Values)(1 << i);
+                if ((usedValues & candidate) == Values.None)
+                    yield return candidate;
+            }
+        }
+
+
+        public bool IsSolved
+        { get { return _EmptySquaresCount == 0; } }
+
+        public bool HasDeadSquares
+        {
+            get
+            {
+                for (int i = 0; i < 9; i++)
+                    for (int j = 0; j < 9; j++)
+                    {
+                        Rows row = (Rows)i;
+                        Columns column = (Columns)j;
+
+                        if (GetCellValue(column, row) != Values.None) continue;
+
+                        if (GetDeniedValues(column, row) == Values.All)
+                            return true;
+                    }
+
+                return false;
+            }
+        }
+
+        public bool Solve()
+        {
+            if (this.IsSolved) return true;
+            if (this.HasDeadSquares) 
+                return false;
+
+            var orderedMoves = this.GenerateMoves().OrderBy(move => move.Score);
+
+            foreach (Move move in orderedMoves)
+            {
+                this.MakeMove(move);
+                if (this.Solve()) return true;
+                this.UnmakeMove(move);
+            }
+
+            return false;
+        }
+
+        
+        private IEnumerable<Move> GenerateMoves()
+        {
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    Rows row = (Rows)i;
+                    Columns column = (Columns)j;
+
+                    if (GetCellValue(column, row) != Values.None) continue;
+
+                    var options = GetValueOptions(column, row).ToArray();
+                    
+                    foreach (Values option in options)
+                    {
+                        var score = options.Count() * 100
+                            + NumberOfSquaresInColumnCouldUseValue(column, option)
+                            + NumberOfSquaresInRowCouldUseValue(row, option)
+                            + NumberOfSquaresInRegionCouldUseValue(column, row, option);
+
+                        yield return new Move(
+                            column,
+                            row,
+                            option,
+                            score
+                            );
+                    }
+                }
+        }
+
+        private int NumberOfSquaresInRowCouldUseValue(Rows row, Values value)
+        {
+            var result = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                Columns c = (Columns)i;
+                if (CouldPutValueInSquare(c, row, value))
+                    result ++;
+            }
+            return result;
+        }
+
+        private int NumberOfSquaresInColumnCouldUseValue(Columns column, Values value)
+        {
+            var result = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                Rows r = (Rows)i;
+                if (CouldPutValueInSquare(column, r, value))
+                    result++;
+            }
+            return result;
+        }
+
+        private int NumberOfSquaresInRegionCouldUseValue(Columns column, Rows row, Values value)
+        {
+            int r = (int)row / 3;
+            int c = (int)column / 3;
+            var result = 0;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j ++ )
+                {
+                    Rows ro = (Rows)(r*3+i);
+                    Columns co = (Columns)(c * 3 + i);
+                    if (CouldPutValueInSquare(co, ro, value)) result++;
+                }
+            
+            return result;
+        }
+
+        private bool CouldPutValueInSquare(Columns column, Rows row, Values value)
+        {
+            int r = (int)row;
+            int c = (int)column;
+            return (
+                ((_RowValues[r] & value) == 0) &&
+                ((_ColumnValues[c] & value) > 0) &&
+                ((_RegionValues[c / 3, r / 3] & value) == 0)
+                );
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 8; i >= 0; i--)
+            {
+                sb.Append("+-+-+-+-+-+-+-+-+-+");
+                sb.Append("\n");
+                sb.Append("|");
+                for (int j = 0; j < 9; j++)
+                {
+                    var v = GetCellValue((Columns)j, (Rows)i);
+                    string s = " ";
+                    if (v == Values.One) s = "1";
+                    if (v == Values.Two) s = "2";
+                    if (v == Values.Three) s = "3";
+                    if (v == Values.Four) s = "4";
+                    if (v == Values.Five) s = "5";
+                    if (v == Values.Six) s = "6";
+                    if (v == Values.Seven) s = "7";
+                    if (v == Values.Eight) s = "8";
+                    if (v == Values.Nine) s = "9";
+                    if (v == Values.None) s = "0";
+
+                    sb.Append(s);
+                    sb.Append("|");
+                  
+                }
+                sb.Append("\n");
+            }
+            sb.Append("+-+-+-+-+-+-+-+-+-+");
+            return sb.ToString();
+        }
+    }
+}
